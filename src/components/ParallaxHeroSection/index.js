2ea3c6390509e083db0519702b0078cfc186ca3a
Babatunde Abdulkareem <babzchizzy27@gmail.com>
2022-09-12 02:21:59 +0100

    Animating the foregroundhero section

--- src/components/ParallaxHeroSection/index.js
+++ src/components/ParallaxHeroSection/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
 import ForegroundHero from './ForegroundHero';
 import BackgroundHero from './BackgroundHero';
@@ -11,11 +11,32 @@ height: 30vw;
 `
 
 function ParallaxHeroSection() {
+    const foregroundHeroRef = useRef();
+    const backgroundHeroRef = useRef();
+
+    useEffect(() => {
+        const onMove = ({ clientX, clientY }) => {
+            foregroundHeroRef.current.moveTo(clientX / 4, clientY / 8)
+        };
+
+        const onLeave = () => {
+            foregroundHeroRef.current.moveTo(0, 0)
+        };
+
+        document.addEventListener("mousemove", onMove);
+        document.body.addEventListener("mouseleave", onLeave);
+        return () => {
+            document.removeEventListener("mousemove", onMove);
+            document.body.removeEventListener("mouseleave", onLeave);
+        };
+
+        
+    }, []);
     return (
         <StyledSVGWrapper>
             <BackgroundHero />
             <MidgroundHero />
-            <ForegroundHero />
+            <ForegroundHero ref={foregroundHeroRef} />
             
         </StyledSVGWrapper>
     );
