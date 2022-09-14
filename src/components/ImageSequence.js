29d36b2fb9f0ee7c574e5ef2a2357465030e8502
Babatunde Abdulkareem <babzchizzy27@gmail.com>
2022-09-14 23:10:33 +0100

    Updating the mockup animation file

---
+++ src/components/ImageSequence.js
@@ -0,0 +1,145 @@
+import image_0001 from "../assets/sequence/0001.jpg";
+import image_0002 from "../assets/sequence/0002.jpg";
+import image_0003 from "../assets/sequence/0003.jpg";
+import image_0004 from "../assets/sequence/0004.jpg";
+import image_0005 from "../assets/sequence/0005.jpg";
+import image_0006 from "../assets/sequence/0006.jpg";
+import image_0007 from "../assets/sequence/0007.jpg";
+import image_0008 from "../assets/sequence/0008.jpg";
+import image_0009 from "../assets/sequence/0009.jpg";
+import image_0010 from "../assets/sequence/0010.jpg";
+import image_0011 from "../assets/sequence/0011.jpg";
+import image_0012 from "../assets/sequence/0012.jpg";
+import image_0013 from "../assets/sequence/0013.jpg";
+import image_0014 from "../assets/sequence/0014.jpg";
+import image_0015 from "../assets/sequence/0015.jpg";
+import image_0016 from "../assets/sequence/0016.jpg";
+import image_0017 from "../assets/sequence/0017.jpg";
+import image_0018 from "../assets/sequence/0018.jpg";
+import image_0019 from "../assets/sequence/0019.jpg";
+import image_0020 from "../assets/sequence/0020.jpg";
+import image_0021 from "../assets/sequence/0021.jpg";
+import image_0022 from "../assets/sequence/0022.jpg";
+import image_0023 from "../assets/sequence/0023.jpg";
+import image_0024 from "../assets/sequence/0024.jpg";
+import image_0025 from "../assets/sequence/0025.jpg";
+import image_0026 from "../assets/sequence/0026.jpg";
+import image_0027 from "../assets/sequence/0027.jpg";
+import image_0028 from "../assets/sequence/0028.jpg";
+import image_0029 from "../assets/sequence/0029.jpg";
+import image_0030 from "../assets/sequence/0030.jpg";
+import image_0031 from "../assets/sequence/0031.jpg";
+import image_0032 from "../assets/sequence/0032.jpg";
+import image_0033 from "../assets/sequence/0033.jpg";
+import image_0034 from "../assets/sequence/0034.jpg";
+import image_0035 from "../assets/sequence/0035.jpg";
+import image_0036 from "../assets/sequence/0036.jpg";
+import image_0037 from "../assets/sequence/0037.jpg";
+import image_0038 from "../assets/sequence/0038.jpg";
+import image_0039 from "../assets/sequence/0039.jpg";
+import image_0040 from "../assets/sequence/0040.jpg";
+import image_0041 from "../assets/sequence/0041.jpg";
+import image_0042 from "../assets/sequence/0042.jpg";
+import image_0043 from "../assets/sequence/0043.jpg";
+import image_0044 from "../assets/sequence/0044.jpg";
+import image_0045 from "../assets/sequence/0045.jpg";
+import image_0046 from "../assets/sequence/0046.jpg";
+import image_0047 from "../assets/sequence/0047.jpg";
+import image_0048 from "../assets/sequence/0048.jpg";
+import image_0049 from "../assets/sequence/0049.jpg";
+import image_0050 from "../assets/sequence/0050.jpg";
+import image_0051 from "../assets/sequence/0051.jpg";
+import image_0052 from "../assets/sequence/0052.jpg";
+import image_0053 from "../assets/sequence/0053.jpg";
+import image_0054 from "../assets/sequence/0054.jpg";
+import image_0055 from "../assets/sequence/0055.jpg";
+import image_0056 from "../assets/sequence/0056.jpg";
+import image_0057 from "../assets/sequence/0057.jpg";
+import image_0058 from "../assets/sequence/0058.jpg";
+import image_0059 from "../assets/sequence/0059.jpg";
+import image_0060 from "../assets/sequence/0060.jpg";
+import image_0061 from "../assets/sequence/0061.jpg";
+import image_0062 from "../assets/sequence/0062.jpg";
+import image_0063 from "../assets/sequence/0063.jpg";
+import image_0064 from "../assets/sequence/0064.jpg";
+import image_0065 from "../assets/sequence/0065.jpg";
+import image_0066 from "../assets/sequence/0066.jpg";
+import image_0067 from "../assets/sequence/0067.jpg";
+import image_0068 from "../assets/sequence/0068.jpg";
+import image_0069 from "../assets/sequence/0069.jpg";
+import image_0070 from "../assets/sequence/0070.jpg";
+import image_0071 from "../assets/sequence/0071.jpg";
+
+export const iphoneimages = [
+  image_0001,
+  image_0002,
+  image_0003,
+  image_0004,
+  image_0005,
+  image_0006,
+  image_0007,
+  image_0008,
+  image_0009,
+  image_0010,
+  image_0011,
+  image_0012,
+  image_0013,
+  image_0014,
+  image_0015,
+  image_0016,
+  image_0017,
+  image_0018,
+  image_0019,
+  image_0020,
+  image_0021,
+  image_0022,
+  image_0023,
+  image_0024,
+  image_0025,
+  image_0026,
+  image_0027,
+  image_0028,
+  image_0029,
+  image_0030,
+  image_0031,
+  image_0032,
+  image_0033,
+  image_0034,
+  image_0035,
+  image_0036,
+  image_0037,
+  image_0038,
+  image_0039,
+  image_0040,
+  image_0041,
+  image_0042,
+  image_0043,
+  image_0044,
+  image_0045,
+  image_0046,
+  image_0047,
+  image_0048,
+  image_0049,
+  image_0050,
+  image_0051,
+  image_0052,
+  image_0053,
+  image_0054,
+  image_0055,
+  image_0056,
+  image_0057,
+  image_0058,
+  image_0059,
+  image_0060,
+  image_0061,
+  image_0062,
+  image_0063,
+  image_0064,
+  image_0065,
+  image_0066,
+  image_0067,
+  image_0068,
+  image_0069,
+  image_0070,
+  image_0071,
+];
